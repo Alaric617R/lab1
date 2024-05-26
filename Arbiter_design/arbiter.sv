830f2975ff55051b0ea3c433c58ad9e6c788c892
alaricyx <alaricyx@umich.edu>
2024-05-26 09:26:33 +0800

    update

--- Arbiter_design/arbiter.sv
+++ Arbiter_design/arbiter.sv
@@ -36,9 +36,9 @@ module arbiterFSM(
         //////////////////////////////////////////////////////
         case (state)
             IDLE: begin
-                if (request[0] == 1'b1 ) begin
+                if (request[2] == 1'b1 ) begin
                     state_next = GRANT_2;
-                end else if (request[1] == 1'b1 && request[0] == 1'b0) begin
+                end else if (request[1] == 1'b1 && request[2] == 1'b0) begin
                     state_next = GRANT_1;
                 end else if (request == 3'b001) begin
                     state_next = GRANT_0;
@@ -47,19 +47,19 @@ module arbiterFSM(
                 end
             end
             GRANT_0: begin
-                if (request[0] == 1'b1) begin
+                if (request[2] == 1'b1) begin
                     $display("here! request:%b, request[0]: %b",request, request[0]);
                     state_next = GRANT_2;
-                end else if (request[1] == 1'b1 && request[0] == 1'b0) begin
+                end else if (request[1] == 1'b1 && request[2] == 1'b0) begin
                     state_next = GRANT_1;
                 end else  begin
                     state_next = GRANT_0;
                 end 
             end
             GRANT_1: begin
-                if (request[2] == 1'b1) begin
+                if (request[0] == 1'b1) begin
                     state_next = GRANT_0;
-                end else if (request[0] == 1'b1 && request[2] == 1'b0) begin
+                end else if (request[2] == 1'b1 && request[0] == 1'b0) begin
                     state_next = GRANT_2;
                 end else begin
                     state_next = GRANT_1;
